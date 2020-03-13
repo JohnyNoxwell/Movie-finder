d61d57d54a933124db3dda7c1939510a2733f0db
Nox <nox@iMac-Nox.local>
2020-03-14 00:48:57 +0200

    frist commit

---
+++ src/views/Home.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="MovieFinder" />
+    <input
+      v-model="search"
+      type="text"
+      class="search"
+      @keyup.enter="searchFilm"
+    />
+    <button @click="searchFilm">Search</button>
+    <h2 v-if="noResults" class="result-alert">NO RESULTS</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      search: "",
+      searching: false,
+      results: null,
+      noResults: false
+    };
+  },
+
+  methods: {
+    searchFilm: function() {
+      this.searching = true;
+      fetch(`http://www.omdbapi.com/?apikey=e082a6c9&s=${this.search}`)
+        .then(res => res.json())
+        .then(res => {
+          if (res.Response == "False") {
+            this.noResults = true;
+            console.log(this.noResults);
+          } else {
+            this.searching = false;
+            this.results = res.results;
+            this.$router.push({
+              name: "Results",
+              params: {
+                items: res.Search
+              }
+            });
+          }
+
+          console.log(res.Response);
+        });
+    }
+  }
+};
+</script>
+<style>
+.home {
+  padding-top: 20vh;
+}
+.search {
+  padding: 10px;
+  margin-right: 10px;
+  /* height: auto; */
+  width: 600px;
+  font-size: 18px;
+}
+.result-alert {
+  color: red;
+}
+button {
+  padding: 10px;
+  font-size: 19px;
+  background: #eee;
+}
+</style>
---
+++ src/views/Results.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="about">
+    <div class="results">
+      <div v-for="item in items" :key="item.param" class="result-item">
+        <img class="result-item__poster" :src="item.Poster" :alt="item.Title" />
+        <h3>{{ item.Title }}</h3>
+        <p>{{ item.Year }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.about {
+  max-width: 1170px;
+  margin: 0 auto;
+}
+.result-item {
+  flex-basis: calc(25% - 20px);
+  box-shadow: 2px 0px 9px -2px rgba(0, 0, 0, 0.75);
+  margin: 20px 10px;
+}
+.result-item img {
+  width: 100%;
+  height: 400px;
+  object-fit: cover;
+}
+.results {
+  display: flex;
+  flex-wrap: wrap;
+  /* justify-content: space-between; */
+}
+</style>
+
+<script>
+export default {
+  name: "Results",
+  props: ["items"]
+};
+</script>
